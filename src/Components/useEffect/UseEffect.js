3c403091b30574de7e58b4cd0efd0c48b75d8882
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-03-06 19:59:59 -0800

    added useEffect

--- src/Components/useEffect/UseEffect.js
+++ src/Components/useEffect/UseEffect.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function UseEffect() {
+
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    setTitle('Good')
+    console.log('hello')
+  }, [title]);
+
   return (
-    <div>UseEffect</div>
+    <div>
+      <h1>{title}</h1>
+      hello world
+    </div>
   )
 }
