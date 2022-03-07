1373b834dbeea18875666a0792cedae6b4a312ec
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-03-06 19:30:54 -0800

    added useref

--- src/Components/useRef/UseRef.js
+++ src/Components/useRef/UseRef.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
 
 export default function UseRef() {
+  const [ title, setTitle ] = useState('')
+  const inputRef = useRef(null);
+
+  const onClick = () => {
+    setTitle(inputRef.current.value)
+    inputRef.current.value = '';
+  };
+
   return (
-    <div>useRef</div>
+    <div>
+      <h1>{title}</h1>
+      <input type='text' placeholder="ex..." ref={inputRef}/>
+      <button onClick={onClick}>
+        Change Name
+      </button>
+    </div>
   )
 }
 
