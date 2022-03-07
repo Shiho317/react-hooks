00014486f77426be55292691b70adaffe7520076
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-03-06 20:53:33 -0800

    added usememo

--- src/Components/useMemo/UseMemo.js
+++ src/Components/useMemo/UseMemo.js
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, {useMemo, useState} from 'react'
 
 export default function UseMemo() {
+  const [ toggle, setToggle ] = useState(false);
+  const [ title, setTitle ] = useState('')
+
+  const onChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const memoValue = (name) => {
+    return 'hello' + name;
+  }
+
+  const memo = useMemo(() => memoValue('susan'), []);
+
   return (
-    <div>UseMemo</div>
+    <div>
+      <h1>{title}</h1>
+      <input type='text' onChange={onChange}/>
+      <h2>{memo}</h2>
+      <button onClick={() => {
+        setToggle(!toggle)
+      }}>Toggle</button>
+      {toggle && <h1>toggle</h1>}
+    </div>
   )
 }
