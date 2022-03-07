7a4a46b20d27f226273badf4a17eb9f5b0d4d94c
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-03-06 20:34:28 -0800

    added useContext

--- src/Components/useContext/UseContext.js
+++ src/Components/useContext/UseContext.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState, createContext } from 'react';
+import Login from './Login';
+import User from './User';
+
+export const AppContext = createContext(null);
 
 export default function UseContext() {
+
+  const [userName, setUserName] = useState('');
+
   return (
-    <div>UseContext</div>
+    <AppContext.Provider value={{userName, setUserName}}>
+      <Login/>
+      <User/>
+    </AppContext.Provider>
   )
 }
