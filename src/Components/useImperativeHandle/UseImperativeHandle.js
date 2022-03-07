e23960967bf9bbcbaafc810de1dd63094c3842d7
Shiho Nagano <ex3jsb.geneg@gmail.com>
2022-03-06 20:17:03 -0800

    added inperative

--- src/Components/useImperativeHandle/UseImperativeHandle.js
+++ src/Components/useImperativeHandle/UseImperativeHandle.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import Button from './Button';
 
 export default function UseImperativeHandle() {
+  const buttonRef = useRef(null);
   return (
-    <div>UseImperativeHandle</div>
+    <div>
+      <button onClick={() => {
+        buttonRef.current.alterToggle();
+      }}>
+        Button From Parent
+      </button>
+      <Button ref={buttonRef}/>
+    </div>
   )
 }
